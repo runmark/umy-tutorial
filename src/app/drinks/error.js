cd33dc5e35103632af96c06cc880c5cb33a45198
maxingmin <ma_xingmin@163.com>
2024-02-25 14:53:42 +0000

    ✨ feat(drinks): error handler page

---
+++ src/app/drinks/error.js
@@ -0,0 +1,11 @@
+'use client';
+
+const error = (error) => {
+    return (
+        <div>
+            {error.error.message}
+        </div>
+    );
+}
+
+export default error;
--- src/app/drinks/page.js
+++ src/app/drinks/page.js
@@ -3,8 +3,13 @@ import Link from "next/link";
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
 
 const getDrinkImages = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error("error when fetch data!!");
+    }
+
     return response.json();
 }
 
