43966572cb58221863c3a51504f5c7d032c6da3f
maxingmin <ma_xingmin@163.com>
2024-03-04 15:17:38 +0000

    drinks responsive image

--- src/app/drinks/[id]/page.js
+++ src/app/drinks/[id]/page.js
@@ -1,10 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
 
-const SingleDrink = ({ params }) => {
+const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
+
+const getSingleDrink = async (id) => {
+    const response = await fetch(`${url}${id}`);
+    if (!response.ok) {
+        throw new Error("sth went wrong");
+    }
+
+    return response.json();
+}
+
+const SingleDrink = async ({ params }) => {
+
+    const data = await getSingleDrink(params.id);
+
+    // console.log(data);
+
+    const title = data?.drinks[0]?.strDrink;
+    const imgSrc = data?.drinks[0]?.strDrinkThumb;
+
+
     return (
         <div>
-            SingleDrink
-            {params.id}
-        </div>
+            <Link href="/drinks"
+                className="btn btn-primary mt-8 mb-12">
+                BACK TO DRINKS
+            </Link>
+
+            <Image src={imgSrc} width={300} height={300}
+                className="w-48 h-48 mb-4 rounded-lg shadow-lg"
+                priority
+                alt={title}
+            />
+
+            <h1 className="text-4xl mb-8">{title}</h1>
+        </div >
     );
 }
 
--- src/app/drinks/page.js
+++ src/app/drinks/page.js
@@ -1,3 +1,5 @@
+import DrinksList from "@/components/DrinksList";
+import Image from "next/image";
 import Link from "next/link";
 
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
@@ -10,19 +12,19 @@ const getDrinkImages = async () => {
         throw new Error("error when fetch data!!");
     }
 
-    return response.json();
+    const data = await response.json();
+
+    return data;
 }
 
 const DrinksPage = async () => {
 
     const data = await getDrinkImages();
-    console.log(data);
+    // console.log(data.drinks);
 
     return (
-        <div className="text-7xl">
-            DrinksPage
-
-            <Link href="/" className="text-2xl">HomePage</Link>
+        <div>
+            <DrinksList drinks={data.drinks} />
         </div>
     );
 }
