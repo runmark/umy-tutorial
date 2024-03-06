c703789df94824d2ff643516894309d2c21965cc
maxingmin <ma_xingmin@163.com>
2024-03-06 07:29:49 +0000

    ✨ feat(prisma-example): task handler

--- src/app/prisma-example/page.js
+++ src/app/prisma-example/page.js
@@ -1,13 +1,41 @@
-import Link from "next/link";
+import prisma from "@/utils/db";
 
-const PrismaExamplePage = () => {
+const prismaHandlers = async () => {
+
+    await prisma.task.create({
+        data: {
+            content: "wake up",
+        },
+    });
+
+    const allTasks = await prisma.task.findMany({
+        orderBy: {
+            createdAt: "desc",
+        }
+    });
+
+    return allTasks;
+
+}
+
+const Prisma = async () => {
+
+    const tasks = await prismaHandlers();
+
     return (
-        <div className="text-7xl">
-            PrismaExamplePage
-
-            <Link href="/" className="text-2xl">HomePage</Link>
+        <div>
+            <h1 className="text-3xl font-medium mb-4">Prisma Example</h1>
+            {
+                tasks.map((task) => {
+                    return (
+                        <h2 key={task.id} className="text-xl py-2">
+                            😄 {task.content}
+                        </h2>
+                    );
+                })
+            }
         </div>
     );
 }
 
-export default PrismaExamplePage;
+export default Prisma;
